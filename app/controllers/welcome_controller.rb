4356db485571fc57447cd31c84a76c26c26c5ea5
tocomi <tocomi@wish.ocn.ne.jp>
2017-03-19 00:00:51 +0900

    add logic to view race list

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,22 @@
 class WelcomeController < ApplicationController
+  require 'uri'
   require_relative './../helpers/netkeiba_scraper'
   def index
     get_race_header()
     @race_list = []
   end
+  
+  def race_list()
+    query = get_query_of_current_url()
+    target = query['target']
+    get_race_list(target)
+  end
+  
+  def get_query_of_current_url()
+    current_url = request.url
+    uri = URI::parse(current_url)
+    query_array = URI::decode_www_form(uri.query)
+    query_hash = Hash[query_array]
+  end
+  
 end
