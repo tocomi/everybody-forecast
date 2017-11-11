798ab94b02ef01297f07f0626d3460d6be9a639a
tocomi <tocomi@wish.ocn.ne.jp>
2017-11-11 18:07:14 +0900

    add vagrant and chef recipe

---
+++ rails_cookbook/site-cookbooks/tocomi_cookbook/metadata.rb
@@ -0,0 +1,7 @@
+name             'tocomi_cookbook'
+maintainer       'tocomi'
+maintainer_email 'NONE'
+license          'All rights reserved'
+description      'A cookbook for everybody_forecast'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
