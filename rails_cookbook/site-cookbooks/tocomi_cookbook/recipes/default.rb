686ffa9eaa13275a378b66baa6398a7f4add33f4
tocomi <tocomi@wish.ocn.ne.jp>
2017-11-11 21:09:37 +0900

    chef recipe

--- rails_cookbook/site-cookbooks/tocomi_cookbook/recipes/default.rb
+++ rails_cookbook/site-cookbooks/tocomi_cookbook/recipes/default.rb
@@ -6,3 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+include_recipe 'tocomi_cookbook::ops_user'
+include_recipe 'tocomi_cookbook::keys'
+include_recipe 'tocomi_cookbook::nginx_setting'
+include_recipe 'tocomi_cookbook::iptables'
