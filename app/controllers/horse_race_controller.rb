714eb49582ca1d431f22295939a227335ac779f6
tocomi <tocomi@wish.ocn.ne.jp>
2018-03-06 02:38:57 +0900

    [WIP] feat: add update logic

--- app/controllers/horse_race_controller.rb
+++ app/controllers/horse_race_controller.rb
@@ -1,9 +1,8 @@
 class HorseRaceController < ApplicationController
   require 'uri'
+  require 'date'
   require_relative './../helpers/netkeiba_scraper'
 
-  # forecast data
-  @forecasts
   # flag for display combobox
   @exists_forecast
   @forecast_select
@@ -31,16 +30,25 @@ class HorseRaceController < ApplicationController
   end
 
   def get_forecast()
-    @forecasts = Forecast.where(race_id: get_race_id)
+    @forecasts = Forecast.where("race_id = ?", get_race_id)
   end
 
   def save_forecast()
-    forecast = Forecast.new(user_id: 1, race_id: params[:race_id], horse_number: params[:horse_number], forecast: params[:forecast])
-    forecast.save 
+    current_forecast = Forecast.find_by(user_id: get_user_id, race_id: params[:race_id].to_i, horse_number: params[:horse_number].to_i)
+    logger.debug("[%s] current_forecast: %s" % [DateTime.now.strftime("%Y/%m/%d %H:%M:%S"), current_forecast.to_s])
+    if current_forecast then
+      current_forecast.update_attributes(forecast: params[:forecast])
+    else
+      Forecast.create(user_id: get_user_id, race_id: params[:race_id], horse_number: params[:horse_number], forecast: params[:forecast])
+    end
   end
 
   def get_race_id
     adjust_target(params[:target]).split("&")[1][-12..-1].to_i
+  end
+
+  def get_user_id
+    1
   end
 
 end
