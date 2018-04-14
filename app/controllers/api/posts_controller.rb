ccb3ad78d0e32ada77daf8264f35fe386ad93a38
tocomi <tocomi@wish.ocn.ne.jp>
2018-04-15 01:42:58 +0900

    implement post method

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,9 +1,16 @@
 class Api::PostsController < ApplicationController
+  protect_from_forgery :except => [:create]
+
   def show
     @posts = Post.where("race_id = ?", params[:id])
                  .joins("INNER JOIN users ON posts.user_id = users.id")
                  .select("users.id, users.display_name, posts.comment, posts.created_at")
                  .order("posts.created_at DESC")
     render 'show', formats: 'json', handlers: 'jbuilder'
+  end
+
+  def create
+    @post = Post.create(race_id: params[:race_id], user_id: params[:user_id], comment: params[:comment])
+    head :no_content
   end
 end
