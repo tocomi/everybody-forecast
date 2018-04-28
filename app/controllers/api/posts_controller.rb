4689e84ae6097f1f5cf2a129d0a976965cf93f79
tocomi <tocomi0112@gmail.com>
2018-04-28 15:28:50 +0900

    change date format

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -2,10 +2,21 @@ class Api::PostsController < ApplicationController
   protect_from_forgery :except => [:create]
 
   def show
-    @posts = Post.where("race_id = ?", params[:id])
+    posts = Post.where("race_id = ?", params[:id])
                  .joins("INNER JOIN users ON posts.user_id = users.id")
                  .select("users.id, users.display_name, posts.comment, posts.created_at")
                  .order("posts.created_at DESC")
+
+    @posts = []
+    posts.each do |each_post|
+      post = {}
+      post[:id] = each_post[:id]
+      post[:display_name] = each_post[:display_name]
+      post[:comment] = each_post[:comment]
+      post[:created_at] = each_post[:created_at].strftime("%m/%d %H:%M")
+      @posts.push(post)
+    end
+
     render 'show', formats: 'json', handlers: 'jbuilder'
   end
 
