be8d92906e58c823c8c95cc5653a1c8d970eca84
tocomi <tocomi0112@gmail.com>
2018-05-04 19:34:23 +0900

    use factorybot

--- spec/factories/horses.rb
+++ spec/factories/horses.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :horse do
-    
+    gate_number 1
+    horse_number 2
+    name 'キタサンブラック'
+    age_handi_jockey '牡5 57 武豊'
+    odds_and_rank '1.9(1人気)'
+
+    initialize_with { new(gate_number, horse_number, name, age_handi_jockey, odds_and_rank) }
   end
 end
--- spec/models/horse_spec.rb
+++ spec/models/horse_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Horse, type: :model do
   it 'gets gate class depends on gate number' do
-    horse = Horse.new(1, 2, 'キタサンブラック', '牡5 57 武豊', '1.9(1人気)')
+    horse = build(:horse)
     expect(horse.get_gate_class).to eq 'horse-table gate-number'
   end
 end
